fb2eae6961443f4f91fc2fb82df8cff1c84bf967
Olli Lupton <oliver.lupton@epfl.ch>
2021-01-12 15:33:04 +0100

    minor fix and cleanup comments

--- scripts/buildNeuron.sh
+++ scripts/buildNeuron.sh
@@ -10,9 +10,9 @@ source ../scripts/environment_${OS_FLAVOUR}.sh
 # Choose which Python version to use
 export PYTHON=$(command -v python3)
 
-# TODO remove some of these packages
-${PYTHON} -m pip install --user --upgrade -r docs/docs_requirements.txt bokeh cython ipython matplotlib mpi4py \
-  pytest scikit-build
+# Install extra dependencies
+${PYTHON} -m pip install --user --upgrade -r docs/docs_requirements.txt bokeh \
+  cython ipython matplotlib mpi4py pytest scikit-build
 
 # Set default compilers, but don't override preset values
 export CC=${CC:-gcc}
@@ -29,23 +29,14 @@ echo CMake \($(command -v cmake)\)
 cmake --version
 ${PYTHON} -c 'import os, sys; os.set_blocking(sys.stdout.fileno(), True)'
 
-# TODO see if this is still needed
-#if [ "$RUNNER_OS" == "macOS" ]; then
-  # Python is not installed as a framework, so we need to writ 'backend: TkAgg' to `matplotlibrc`.
-  # Since we are in a virtual environment, we cannot use `$HOME/matplotlibrc`
-  # The following solution is generic and relies on `matplotlib.__file__` to know where to append backend setup.
-  #$PYTHON -c "import os,matplotlib; f =open(os.path.join(os.path.dirname(matplotlib.__file__), 'mpl-data/matplotlibrc'),'a'); f.write('backend: TkAgg');f.close();"
-#fi;
-export CMAKE_OPTION="-DNRN_ENABLE_BINARY_SPECIAL=ON -DNRN_ENABLE_MPI=ON -DNRN_ENABLE_INTERVIEWS=ON -DNRN_ENABLE_CORENEURON=ON -DPYTHON_EXECUTABLE=${PYTHON}"
+export CMAKE_OPTION="-DNRN_ENABLE_BINARY_SPECIAL=ON -DNRN_ENABLE_MPI=ON \
+ -DNRN_ENABLE_INTERVIEWS=ON -DNRN_ENABLE_CORENEURON=ON \
+ -DPYTHON_EXECUTABLE=${PYTHON}"
 mkdir build
 pushd build
 echo "Building with: ${CMAKE_OPTION}"
 cmake ${CMAKE_OPTION}  -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DNRN_ENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ..
 cmake --build . -- -j
-# TODO see if this is still needed
-if [ "$RUNNER_OS" == "macOS" ]; then
-  echo $'[install]\nprefix='>src/nrnpython/setup.cfg
-fi
 make install
 # Make sure the installed files can be found
 export PATH=${INSTALL_DIR}/bin:${PATH}
--- scripts/install_debian_ubuntu:18.04.sh
+++ scripts/install_debian_ubuntu:18.04.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # As with CentOS7, the default Git is too old for the GitHub Actions checkout
 # module. Install a newer one.
+apt-get update
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:git-core/ppa
