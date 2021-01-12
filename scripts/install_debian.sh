88d73183e8e1efecea5ff97b1c2e2ee21b02f5eb
Olli Lupton <oliver.lupton@epfl.ch>
2021-01-12 17:46:42 +0100

    Remove global upgrades; the targeted installs should upgrade dependencies too

--- scripts/install_debian.sh
+++ scripts/install_debian.sh
@@ -13,7 +13,6 @@ fi
 # Use sudo if its available (typically no inside Docker and yes outside)
 SUDO=`command -v sudo || true`
 ${SUDO} apt-get update
-${SUDO} apt-get upgrade -y
 ${SUDO} apt-get install -y bison cmake doxygen flex git libncurses-dev \
   libopenmpi-dev libx11-dev libxcomposite-dev openmpi-bin python3-numpy \
   python3-pip python3-setuptools python3-wheel sudo
